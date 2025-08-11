ba633be62793a224deb655a8c254a210d4d95253
Vaishnavi Deepak Sonawane <vaishnavisonawane1118@gmail.com>
2025-08-11 22:35:42 +0530

    initial commit

---
+++ ArrayList/Array_Reverse.java
@@ -0,0 +1,29 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Array_Reverse {
+    static void reverselist(ArrayList<Integer> list){
+        int i=0, j=list.size()-1;
+        while(i<j) {
+            Integer temp=Integer.valueOf(list.get(i));
+            list.set(i, list.get(j));
+            list.set(j,temp);
+            i++;
+            j--;
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(0);
+        list.add(10);
+        list.add(3);
+        list.add(5);
+        list.add(22);
+        list.add(10);
+        System.out.println("Original List: "+list);
+
+        reverselist(list);
+        System.out.println("Reversed List: "+list);
+    }
+}
