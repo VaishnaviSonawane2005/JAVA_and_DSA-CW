50e0d4faa2fbea1b59b819cc799f38ee55584b47
Vaishnavi Deepak Sonawane <vaishnavisonawane1118@gmail.com>
2026-01-09 11:31:18 +0530

    initial commit

--- Arrays_2D/Array2D_MatrixMultiplication.java
+++ Arrays_2D/Array2D_MatrixMultiplication.java
@@ -35,7 +35,7 @@ public class Array2D_MatrixMultiplication {
         int r1= sc.nextInt();
         int c1= sc.nextInt();
         int[][] a=new int[r1][c1];
-        System.out.println("Enter the 2 Dimensional Array having "+r1*c1+" elements of Martix 1:");
+        System.out.println("Enter the 2 Dimensional Array having "+r1*c1+" elements of Matrix 1:");
         for (int i = 0; i < r1; i++) {
             for (int j = 0; j < c1; j++) {
                 a[i][j]= sc.nextInt();
@@ -45,7 +45,7 @@ public class Array2D_MatrixMultiplication {
         int r2= sc.nextInt();
         int c2= sc.nextInt();
         int[][] b=new int[r2][c2];
-        System.out.println("Enter the 2 Dimensional Array having "+r2*c2+" elements of Martix 1:");
+        System.out.println("Enter the 2 Dimensional Array having "+r2*c2+" elements of Matrix 2:");
         for (int i = 0; i < r2; i++) {
             for (int j = 0; j < c2; j++) {
                 b[i][j]= sc.nextInt();
--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,4 +1,56 @@
 package Sorting;
 
 public class MergeSort {
+    static void merge(int[] arr, int l, int mid, int r){
+        int n1 = mid-l+1;
+        int n2 = r - mid;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+        int i, j, k;
+        for (i = 0; i < n1; i++)
+            left[i] = arr[l + i];
+        for (j = 0; j < n2; j++)
+            right[j] = arr[mid + 1 + j];
+        i = 0;
+        j = 0;
+        k = l;
+        while(i < n1 && j < n2){
+            if(left[i] <= right[j])
+                arr[k++] = left[i++];
+            else
+                arr[k++] = right[j++];
+        }
+        while(i < n1)
+            arr[k++] = left[i++];
+        while(j < n2)
+            arr[k++] = right[j++];
+
+    }
+
+    static void divide_arr(int[] arr, int l, int r){
+        int mid = (l + r) / 2;
+        if(l >= r){
+            return;
+        }
+        divide_arr(arr, l, mid);
+        divide_arr(arr, mid+1, r);
+        merge(arr, l, mid, r);
+    }
+
+    static int[] printArray(int[] arr){
+        for (int n : arr){
+            System.out.print(n+" ");
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = {6, 4, 2, 8, 9, 5};
+        System.out.println("Array before sorting: ");
+        printArray(arr);
+        System.out.println();
+        divide_arr(arr, 0, arr.length-1);
+        System.out.println("Sorted Array using Merge Sort is: ");
+        printArray(arr);
+        
+    }
 }
