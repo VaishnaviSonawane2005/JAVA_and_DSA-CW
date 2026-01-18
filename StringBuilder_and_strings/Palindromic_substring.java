ef7f59924c6ad1cc6616ca7024d778d2a0192ffd
Vaishnavi Deepak Sonawane <vaishnavisonawane1118@gmail.com>
2026-01-18 20:13:22 +0530

    initial commit

--- StringBuilder_and_strings/Palindromic_substring.java
+++ StringBuilder_and_strings/Palindromic_substring.java
@@ -3,17 +3,30 @@ package StringBuilder_and_strings;
 import java.util.Scanner;
 
 public class Palindromic_substring {
+    static boolean isPalindrome(String str){
+        int i = 0, j = str.length()-1;
+        while(i <= j){
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
     public static void main(String[] args) {
-
-        // Method I
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a String: ");
-        StringBuilder str = new StringBuilder(sc.nextLine());
-        String org = str.toString();
-        String reversed = str.reverse().toString();
-        if (org.equals(reversed))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not a Palindrome");
+        String str = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i+1; j <= str.length(); j++) {
+                if (isPalindrome(str.substring(i, j))) {
+                    System.out.print(str.substring(i, j)+" ");
+                    count++;
+                }
+            }
+        }
+        System.out.println("\nThe no. of palindromic substrings are: "+count);
     }
 }
